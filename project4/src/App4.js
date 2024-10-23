212b13b920967fdd55c7222c165a0a71fd1de8cc
SaifMahabubArnob <mahabub.web404@gmail.com>
2024-10-23 13:33:55 +0600

    23 oct 2024

--- project4/src/App4.js
+++ project4/src/App4.js
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function App4() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        getData()
-      }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    function getData(){
-         axios.get(`https://jsonplaceholder.typicode.com/posts`)
-         .then(result => {
-            const myData = result.data;
-            //console.log(myData);
-            setData(myData);
-         })
-    }
-    
-    console.log(data);
+  function getData() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((result) => {
+      const myData = result.data;
+      //console.log(myData);
+      setData(myData);
+    });
+  }
+
+  //console.log(data);
 
   return (
     <>
-        <div>Data Display</div>
-        {data.map(item => 
-            console.log(item.id)
-        )}
+      <div>Data Display</div>
+
+      {data.map(item => {
+        //console.log(item.id)
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
---
+++ project5/php_api(htdocs_file)/insert.php
@@ -0,0 +1,23 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: *");
+    echo "Testing";
+?>
+
+<?php
+    include_once ("dbconfig.php");
+
+    $user = json_decode(file_get_contents('php://input'));
+
+    $name = $user->name;
+    $email = $user->email;
+    $mobile = $user->mobile;
+
+    $db->query("INSERT INTO users(id, name, email, mobile) values(null, '$name', '$email', '$mobile')");
+
+    //echo "INSERT INTO users(id, name, email, mobile, created_at) values(null, '$name', '$email', '$mobile')";
+
+    if($db->affected_rows){
+        echo "Inserted";
+    }
+?>
